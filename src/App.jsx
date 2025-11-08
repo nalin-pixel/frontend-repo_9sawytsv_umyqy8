ed960fd6e864e2ff8ac14e77a4184ab77b2d4798
Flames <vibe-coder@flames.com>
2025-11-08 00:05:08 +0000

    Update project prj_9sawytsv - 2025-11-08T00:05:07.805Z

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Inventory from './components/Inventory';
 import RequestForm from './components/RequestForm';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen bg-white text-gray-900">
+    <div className="min-h-screen bg-white text-gray-900 flex flex-col">
       <Navbar />
-      <main>
+      <main className="flex-1">
         <Hero />
         <Inventory />
         <RequestForm />
       </main>
+      <Footer />
     </div>
   );
 }
